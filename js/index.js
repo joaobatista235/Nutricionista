8c42a208b6bf48939bf29c32da37df1b8e2f2058
joaobatista235 <juniorbatista0404@gmail.com>
2023-02-07 13:25:16 -0300

    Projeto alura finalizado

---
+++ js/index.js
@@ -0,0 +1,62 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValido = validaAltura(altura);
+
+    if(!pesoEhValido){
+        console.log("Peso invalido");
+        pesoEhValido = false;
+        tdImc.textContent = "peso Invalido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(!alturaEhValido){
+        console.log("altura invalido");
+        alturaEhValido = false;
+        tdImc.textContent = "altura Invalido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(alturaEhValido && pesoEhValido){
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }else{
+        return false
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 3.0){
+        return true;
+    }else{
+        return false
+    }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
